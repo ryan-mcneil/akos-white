19bfffbe895a5157c021bc3def988950df564f9d
Ryan McNeil <ryan@journeyapps.com>
2020-01-05 14:01:54 -0700

    add logos

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div>
-      Hi John. This is going to be awesome. Stay Tuned.
-    </div>
-  );
+import Logo from '../src/assets/AKoS_Logo-Black_1.png';
+
+class App extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+
+    return (
+      <div>
+        <img src={Logo} className="main-logo"/>
+        <h1 className="content">Stay Tuned.</h1> 
+      </div>
+    );
+  }
 }
 
 export default App;
