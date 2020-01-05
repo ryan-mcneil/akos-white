921f0e90833e7eba51aa9e55d42ce60abb77e3e3
Ryan McNeil <ryan@journeyapps.com>
2020-01-05 13:08:11 -0700

    Updates

---
+++ precache-manifest.bdb6c892a5c7f68db55adca29a8114ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b2cb60e727002ee0be9909a609504a6",
+    "url": "/akos-white/index.html"
+  },
+  {
+    "revision": "7d73be5bf53bd750eb0e",
+    "url": "/akos-white/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "9852e657f1657d5848f5",
+    "url": "/akos-white/static/js/2.80db548d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/akos-white/static/js/2.80db548d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7d73be5bf53bd750eb0e",
+    "url": "/akos-white/static/js/main.74bc34f8.chunk.js"
+  },
+  {
+    "revision": "dd62253e740e08cd65e2",
+    "url": "/akos-white/static/js/runtime-main.654222b0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/akos-white/static/media/logo.5d5d9eef.svg"
+  }
+]);
