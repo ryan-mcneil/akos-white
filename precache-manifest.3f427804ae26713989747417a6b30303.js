14f1a2e3f2234a3a5602571a8de7d5f4c62ebefa
Ryan McNeil <ryan@journeyapps.com>
2020-01-19 23:26:22 -0700

    Updates

--- precache-manifest.3f427804ae26713989747417a6b30303.js
+++ precache-manifest.3f427804ae26713989747417a6b30303.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "49d1d8c2b87016bd9fde9fb3217d4a2f",
+    "revision": "1d6fa75f06dffd92699ece3f3c02158c",
     "url": "/akos-white/index.html"
   },
   {
-    "revision": "d848c6eebfdb744bbbcc",
-    "url": "/akos-white/static/css/main.2cd48aff.chunk.css"
+    "revision": "71cf558e4914c63c3b7b",
+    "url": "/akos-white/static/css/main.aec4ca3d.chunk.css"
   },
   {
     "revision": "46dbcfc7d3e7f17b03f5",
@@ -16,7 +16,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/akos-white/static/js/2.bd8fea23.chunk.js.LICENSE"
   },
   {
-    "revision": "d848c6eebfdb744bbbcc",
+    "revision": "71cf558e4914c63c3b7b",
     "url": "/akos-white/static/js/main.33bd8486.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/akos-white/precache-manifest.8ab1ca0ac4da346e11d89b479fea6c82.js"
+  "/akos-white/precache-manifest.3f427804ae26713989747417a6b30303.js"
 );
 
 self.addEventListener('message', (event) => {
